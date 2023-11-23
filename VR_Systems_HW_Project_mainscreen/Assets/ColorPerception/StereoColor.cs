e8b2efe689ace07c93a1f052f598898f437e948e
Denhonator <elmeri.uotila@gmail.com>
2023-11-23 16:32:21 +0200

    Color changing, image loading

--- VR_Systems_HW_Project_mainscreen/Assets/ColorPerception/StereoColor.cs
+++ VR_Systems_HW_Project_mainscreen/Assets/ColorPerception/StereoColor.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class StereoColor : MonoBehaviour
 {
@@ -21,9 +23,6 @@ public class StereoColor : MonoBehaviour
             return;
         init = true;
         main = true;
-
-        leftTest.material.color = new Color(1,1,0.8f);
-        rightTest.material.color = new Color(1,1,0.8f);
 
         GameObject right = Instantiate(transform.gameObject, null);
         right.SetLayerRecursively(rightTest.gameObject.layer);
@@ -62,6 +61,72 @@ public class StereoColor : MonoBehaviour
                 state = 0;
                 leftLight.color = new Color(1, 1, 0.8f);
                 rightLight.color = Color.white;
+            }
+        }
+    }
+
+    public void SetColorLeft(string color)
+    {
+        if(color == null) return;
+        leftTest.sharedMaterial.color = StringToColor(color);
+    }
+
+    public void SetColorRight(string color)
+    {
+        if(color == null) return;
+        rightTest.sharedMaterial.color = StringToColor(color);
+    }
+
+    public void SetLightColorLeft(string color)
+    {
+        if(color == null) return;
+        leftLight.color = StringToColor(color);
+    }
+
+    public void SetLightColorRight(string color)
+    {
+        if(color == null) return;
+        rightLight.color = StringToColor(color);
+    }
+
+    Color StringToColor(string color)
+    {
+        if (color[0] == '#')
+        {
+            Color newCol = Color.white;
+            if (ColorUtility.TryParseHtmlString(color, out newCol))
+            {
+                return newCol;
+            }
+        }
+        string[] rgb = color.Split(',');
+        return new Color(int.Parse(rgb[0]) / 255f, int.Parse(rgb[1]) / 255f, int.Parse(rgb[2]) / 255f);
+    }
+
+    public void LoadImage(string name)
+    {
+        StartCoroutine(GetImageAsync(name));
+    }
+
+    IEnumerator GetImageAsync(string name)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(name))
+        {
+            yield return uwr.SendWebRequest();
+
+            if (uwr.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(uwr.error);
+            }
+            else
+            {
+                // Get downloaded asset bundle
+                Texture2D texture = DownloadHandlerTexture.GetContent(uwr);
+                leftTest.sharedMaterial.SetTexture("_Texture2D", texture);
+                leftTest.transform.localScale = new Vector3(0.2f, 1f, 0.2f * texture.height / texture.width);
+                rightTest.sharedMaterial.SetTexture("_Texture2D", texture);
+                rightTest.transform.localScale = new Vector3(0.2f, 1f, 0.2f * texture.height / texture.width);
+                Debug.Log("Loaded image");
             }
         }
     }
