6719d217579ca5611695d03fc94e749617dfeaec
Denhonator <elmeri.uotila@gmail.com>
2023-11-24 14:33:51 +0200

    Always save before quitting

--- VR_Systems_HW_Project_mainscreen/Assets/customScripts/Quit.cs
+++ VR_Systems_HW_Project_mainscreen/Assets/customScripts/Quit.cs
@@ -49,6 +49,11 @@ public class Quit : MonoBehaviour
         }
     }
 
+    private void OnApplicationQuit()
+    {
+        Leaderboard.Instance.Save();
+    }
+
     // Function to quit the game
     void QuitGame()
     {
